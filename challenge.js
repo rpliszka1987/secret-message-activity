06cb0e740c98b80367747a364d84d5f1d36544de
Robert Pliszka <robplisz@gmail.com>
2022-10-18 17:43:11 -0400

    Added comments to the challenge.js to explain code

--- challenge.js
+++ challenge.js
@@ -1,3 +1,6 @@
+// Secret message activity Code Academy
+
+// Original Array
 let secretMessage = [
   "Learning",
   "is",
@@ -25,24 +28,32 @@ let secretMessage = [
   "JavaScript",
 ];
 
+// Logs length of the array
 console.log(secretMessage.length);
 
+// Deletes last item in the array
 secretMessage.pop();
 
 console.log(secretMessage.length);
 
+// Added to end of array
 secretMessage.push("to", "Program");
 
 console.log(secretMessage.length);
 
+// Finds index of 'easily' and replaces it with 'right'
 secretMessage[secretMessage.indexOf("easily")] = "right";
 
+// Deletes first item in array
 secretMessage.shift();
 
+// Adds first item to the array
 secretMessage.unshift("Programming");
 
 console.log(secretMessage.indexOf("get"));
 
+// Removes 5 items from array after index 6 and replaces with 'know'
 secretMessage.splice(6, 5, "know");
 
+// Prints out secret message
 console.log(secretMessage.join(" "));
